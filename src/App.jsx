26c71d0c6fab7047a89e5abb23f31fcc4512b59e
joaquin-daruich <joaquindaruichh@gmail.com>
2025-05-15 16:56:32 -0300

    San Pedro

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css'
+import {  Route, Routes } from 'react-router-dom';
+
+import Inicio from './Inicio';
+
+
+
+  const App =  () => {
+    
+
+
+
+  return (
+   
+    
+    <>
+      <Routes>
+      <Route path="/" element={<Inicio></Inicio>}/>
+        {/* Ruta con slugs dinámicos */}
+      <Route path="/:segmento1/:segmento2/:segmento3" element={<Inicio />} />
+      </Routes>
+
+      
+    
+</>
+  );
+}
+
+export default App;
